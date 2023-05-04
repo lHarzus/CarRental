ccf4655d6eaf9e3baeea2348b9c16e1e5c609c91
Ruben <rubnitos@hotmail.com>
2023-05-04 13:58:30 +0100

    Last commit

--- client/src/components/profile/Orders.js
+++ client/src/components/profile/Orders.js
@@ -2,92 +2,127 @@ import React, { Fragment, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCars } from "../../actions/cars";
+import { addHistory, deleteOrder } from "../../actions/profile";
+import spinner from "../../images/loading.gif";
 
-const Orders = ({ orders, history, cars: { cars }, getCars }) => {
+const Orders = ({
+  addHistory,
+  deleteOrder,
+  orders,
+  history,
+  cars: { cars, loading },
+  getCars,
+}) => {
   const [orderType, setOrderType] = useState("current");
   useEffect(() => {
     getCars();
+    orders.map((order) => {
+      if (Date.now() > new Date(order.dropoff.date)) {
+        addHistory(order);
+        deleteOrder(order._id);
+      }
+    });
   }, []);
-  return (
-    <Fragment>
-      <div className="orders-buttons">
-        <button className="btn" onClick={() => setOrderType("current")}>
-          Current
-        </button>
-        <button className="btn" onClick={() => setOrderType("history")}>
-          History
-        </button>
-      </div>
+  if (loading) {
+    return (
       <div>
-        {orderType === "current" ? (
-          <Fragment>
-            {orders.length > 0 ? (
-              <Fragment>
-                {orders.map((o, i) => (
-                  <div className="order" key={i}>
-                    <h3>Car:</h3>
-                    <p>
-                      <span class="tab"></span>
-                      {cars.filter((c) => c._id === o.car)[0].brand}{" "}
-                      {cars.filter((c) => c._id === o.car)[0].model}
-                    </p>
-                    <h3>Pick-Up:</h3>
-                    <p>
-                      <span class="tab"></span>
-                      Location: {o.pickup.place}
-                    </p>
-                    <p>
-                      <span class="tab"></span>
-                      Date: {o.pickup.date}
-                    </p>
-                  </div>
-                ))}
-              </Fragment>
-            ) : (
-              <h3>Orders is empty</h3>
-            )}
-          </Fragment>
-        ) : (
-          <Fragment>
-            {history.length > 0 ? (
-              <Fragment>
-                {history.map((h, i) => (
-                  <div className="order" key={i}>
-                    <h3>Car:</h3>
-                    <p>
-                      <span class="tab"></span>
-                      {cars.filter((h) => h._id === h.car)[0].brand}{" "}
-                      {cars.filter((h) => h._id === h.car)[0].model}
-                    </p>
-                    <h3>Pick-Up:</h3>
-                    <p>
-                      <span class="tab"></span>
-                      Location: {h.pickup.place}
-                    </p>
-                    <p>
-                      <span class="tab"></span>
-                      Date: {h.pickup.date}
-                    </p>
-                  </div>
-                ))}
-              </Fragment>
-            ) : (
-              <h3>History is empty</h3>
-            )}
-          </Fragment>
-        )}
+        <img src={spinner}></img>
       </div>
-    </Fragment>
-  );
+    );
+  } else {
+    return (
+      <Fragment>
+        <div className="orders-buttons">
+          <button className="btn" onClick={() => setOrderType("current")}>
+            Current
+          </button>
+          <button className="btn" onClick={() => setOrderType("history")}>
+            History
+          </button>
+        </div>
+        <div>
+          {orderType === "current" ? (
+            <Fragment>
+              {orders.length > 0 ? (
+                <Fragment>
+                  {orders.map((o, i) => (
+                    <div className="order" key={i}>
+                      <h3>Car:</h3>
+                      <p>
+                        <span className="tab"></span>
+                        {cars.filter((c) => c._id === o.car).length > 0
+                          ? cars.filter((c) => c._id === o.car)[0].brand
+                          : "Not Found"}{" "}
+                        {cars.filter((c) => c._id === o.car).length > 0
+                          ? cars.filter((c) => c._id === o.car)[0].model
+                          : "Not Found"}
+                      </p>
+                      <h3>Pick-Up:</h3>
+                      <p>
+                        <span className="tab"></span>
+                        Location: {o.pickup.place}
+                      </p>
+                      <p>
+                        <span className="tab"></span>
+                        Date: {o.pickup.date}
+                      </p>
+                    </div>
+                  ))}
+                </Fragment>
+              ) : (
+                <h3>Orders is empty</h3>
+              )}
+            </Fragment>
+          ) : (
+            <Fragment>
+              {history.length > 0 ? (
+                <Fragment>
+                  {history.map((h, i) => (
+                    <div className="order" key={i}>
+                      <h3>Car:</h3>
+                      <p>
+                        <span className="tab"></span>
+                        {cars.filter((c) => c._id === h.car).length > 0
+                          ? cars.filter((c) => c._id === h.car)[0].brand
+                          : "Not Found"}{" "}
+                        {cars.filter((c) => c._id === h.car).length > 0
+                          ? cars.filter((c) => c._id === h.car)[0].model
+                          : "Not Found"}
+                      </p>
+                      <h3>Pick-Up:</h3>
+                      <p>
+                        <span className="tab"></span>
+                        Location: {h.pickup.place}
+                      </p>
+                      <p>
+                        <span className="tab"></span>
+                        Date: {h.pickup.date}
+                      </p>
+                    </div>
+                  ))}
+                </Fragment>
+              ) : (
+                <h3>History is empty</h3>
+              )}
+            </Fragment>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
 };
 
 Orders.propTypes = {
   getCars: PropTypes.func.isRequired,
   cars: PropTypes.object.isRequired,
+  addHistory: PropTypes.func.isRequired,
+  deleteOrder: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   cars: state.cars,
 });
 
-export default connect(mapStateToProps, { getCars })(Orders);
+export default connect(mapStateToProps, { getCars, addHistory, deleteOrder })(
+  Orders
+);
